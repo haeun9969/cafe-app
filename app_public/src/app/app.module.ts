43c894375e7c5c54ecbae57a0c828aded1f34bb8
unknown <haeun9969@naver.com>
2021-12-17 02:08:14 +0900

    make it better

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -60,12 +60,7 @@ RouterModule.forRoot([
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [FrameworkComponent]
--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  apiBaseUrl: 'https://loc8rhaeun.herokuapp.com/api',
   production: true
 };
